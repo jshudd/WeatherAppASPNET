2c8f43fc0243667e9af416856f2009a7ae1e8e29
Jeremy <jshudd@gmail.com>
2020-12-01 14:13:39 -0600

    initialized project; set up gitignore

---
+++ WeatherAppASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeatherAppASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
